9aa5c098e379acceb7f1f76487d2371a2e546d8f
igroman4ik-pc <ni4000009@gmail.com>
2025-05-17 22:43:56 +0300

    feat: update AppHeader for correct userName update AppHeaderUI NavLink (add isActive)

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,10 @@
+import { userDataSelector } from '@selectors';
 import { AppHeaderUI } from '@ui';
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const user = useSelector(userDataSelector);
+
+  return <AppHeaderUI userName={(user && user.name) || ''} />;
+};
--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -5,7 +5,7 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 
@@ -13,27 +13,40 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <Link to={'/'} className={styles.link}>
+        <NavLink
+          to={'/'}
+          className={({ isActive }) =>
+            `${styles.link} ${isActive && styles.link_active}`
+          }
+          end
+        >
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </Link>
-        <Link to={'feed'} className={styles.link}>
+        </NavLink>
+        <NavLink
+          to={'/feed'}
+          className={({ isActive }) =>
+            `${styles.link} ${isActive && styles.link_active}`
+          }
+        >
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </Link>
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
       </div>
-      <Link
+      <NavLink
         to={'/profile'}
-        className={`${styles.link} ${styles.link_position_last}`}
+        className={({ isActive }) =>
+          `${styles.link} ${styles.link_position_last} ${isActive && styles.link_active}`
+        }
       >
         <ProfileIcon type={'primary'} />
         <p className='text text_type_main-default ml-2'>
           {userName || 'Личный кабинет'}
         </p>
-      </Link>
+      </NavLink>
     </nav>
   </header>
 );
